a4466e4db460a074c44443a744da1d0abe607f2c
amrit <iamamrit27@gmail.com>
2025-05-17 20:11:11 +0530

    feat: implement session management in login page to redirect authenticated users

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import SignIn from "@/components/sign-in";
+import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
 
 export default function Login() {
+  const { 
+    data: session, 
+  } = authClient.useSession();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <main className="flex flex-col justify-center items-center min-h-screen p-4">
       <div className="w-full max-w-md">
