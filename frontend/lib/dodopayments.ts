6b3cba1de89e597782a574ba4b41d8599b31e50b
amrit <iamamrit27@gmail.com>
2025-05-26 10:18:03 +0530

    update var name

--- frontend/lib/dodopayments.ts
+++ frontend/lib/dodopayments.ts
@@ -1,6 +1,6 @@
 import DodoPayments from 'dodopayments';
 
 export const dodopayments = new DodoPayments({
-  bearerToken: process.env.NODE_ENV === 'development' ? process.env.DODO_PAYMENTS_API_KEY_TEST : process.env.DODO_PAYMENTS_API_KEY_LIVE,
+  bearerToken: process.env.NODE_ENV === 'development' ? process.env.DODO_PAYMENTS_API_KEY : process.env.DODO_PAYMENTS_API_KEY_LIVE,
   environment: process.env.NODE_ENV === 'development' ? 'test_mode' : 'live_mode',
 });
